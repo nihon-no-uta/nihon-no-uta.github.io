689b007e52fc0f5ec66bf5e2a0ea8c7456ab3606
atharva naik <atharvanaik2018@gmail.com>
2020-10-16 14:43:59 +0530

    heroku specific settings

--- jll/settings.py
+++ jll/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
-import django-heroku # FOR HEROKU ONLY
+import django_heroku # FOR HEROKU ONLY
 import os
 import argparse
 
